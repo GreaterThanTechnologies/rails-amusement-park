d8ae47331270de32568d9c4648eda80576c4e3f1
prodigysurge <boatinglife6@yahoo.com>
2021-03-23 19:40:52 -0400

    commit tree d8f23027dae71191449095520cb9e0fec72d9089

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :attractions, only: [:new, :create, :edit, :update], controller: 'admin/attractions'
+  resources :attractions, only: [:index, :show]
+  resources :rides
+
+  namespace :admin do
+    resources :attractions
+
+    post '/logout' => "sessions#destroy"
+  end
+
+
+  get '/signin' => "sessions#new"
+  post '/signin' => "sessions#create"
+  post '/logout' => "sessions#destroy"
+
+  get '/signup' => "users#new"
+  post '/signup' => "users#create"
+
+
+  root 'users#home'
 end
